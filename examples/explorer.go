8060ea5824a69bc04ddb298cdd77deb54b0f7d4c
Valentin Durand <vDurand@users.noreply.github.com>
2017-05-10 09:06:47 +0700

    Update explorer.go

--- examples/explorer.go
+++ examples/explorer.go
@@ -10,8 +10,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/paypal/gatt"
-	"github.com/paypal/gatt/examples/option"
+	"github.com/vDurand/gatt"
+	"github.com/vDurand/gatt/examples/option"
 )
 
 var done = make(chan struct{})
