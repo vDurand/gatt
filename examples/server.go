6f78f764b230d78197fcd3cedd0ca67e5be78f42
Valentin Durand <vDurand@users.noreply.github.com>
2017-05-10 09:06:37 +0700

    Update server.go

--- examples/server.go
+++ examples/server.go
@@ -6,9 +6,9 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/paypal/gatt"
-	"github.com/paypal/gatt/examples/option"
-	"github.com/paypal/gatt/examples/service"
+	"github.com/vDurand/gatt"
+	"github.com/vDurand/gatt/examples/option"
+	"github.com/vDurand/gatt/examples/service"
 )
 
 func main() {
