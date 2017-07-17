0e635a101e9fbfd2d4ecefbb425d6c92351d7bd8
Valentin Durand <vDurand@users.noreply.github.com>
2017-07-17 08:59:59 +0700

    Update discoverer.go

--- examples/discoverer.go
+++ examples/discoverer.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/vDurand/gatt"
-	"github.com/vDurand/gatt/examples/option"
+	"github.com/Frontware/gatt"
+	"github.com/Frontware/gatt/examples/option"
 )
 
 func onStateChanged(d gatt.Device, s gatt.State) {
